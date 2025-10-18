affdaac62fe174f212dfdf52f604292030d3d7b6
Subham K. <me@growly.gg>
2025-10-18 09:53:48 -0700

    feat: initial app structure

---
+++ app/sportsclips/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  sportsclips
+//
+//  Created by Subham on 10/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
